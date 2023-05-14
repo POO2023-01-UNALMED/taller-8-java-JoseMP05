0568b7217cc69da95791e4bacbd7cbb8c750b55b
Jose Miguel Pulgarin A <jjosemii64@gmail.com>
2023-05-13 20:08:39 -0500

    corrigiendo rutas

---
+++ src/main/java/futbol/Main.java
@@ -0,0 +1,11 @@
+package futbol;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Futbolista f = new Jugador("Alejo", 20, "Delantero", (short)0, (byte)0);
+		System.out.println(f);
+	}
+
+}
